6bbf3b4e1139230bf1f4ff3fde2409d82a539106
Razgore <gb942tm@gmail.com>
2017-05-01 17:17:03 +0200

    screenSizeTest, getStatusBarHeightBranchTrue, getStatusBarHeightBranchFalse

--- outlay/app/src/main/java/test/TestDeviceUtils.java
+++ outlay/app/src/main/java/test/TestDeviceUtils.java
@@ -3,21 +3,15 @@ package test;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
-import android.inputmethodservice.InputMethodService;
-import android.util.DisplayMetrics;
+import android.support.annotation.DimenRes;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-
 import junit.framework.Assert;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
-
 import app.outlay.utils.DeviceUtils;
-import app.outlay.utils.ResourceUtils;
-
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -40,18 +34,34 @@ public class TestDeviceUtils {
     public void setup(){
 
     }
-
+    //cannot be tested because getMetrics returns void
     @Test
     public void screenSizeTest(){
-
     }
     @Test
     public void getStatusBarHeightBranchTrue(){
+        @DimenRes
+        int dim = 1;
+        when(context.getTheme()).thenReturn(theme);
+        when(context.getResources()).thenReturn(resources);
+        when(resources.getIdentifier("status_bar_height", "dimen", "android")).thenReturn(1);
+        when(resources.getDimensionPixelSize(dim)).thenReturn(1);
+        Assert.assertEquals(1, DeviceUtils.getStatusBarHeight(context));
+
+        verify(resources, times(1)).getDimensionPixelSize(dim);
 
     }
     @Test
     public void getStatusBarHeightBranchFalse(){
+        @DimenRes
+        int dim = 1;
+        when(context.getTheme()).thenReturn(theme);
+        when(context.getResources()).thenReturn(resources);
+        when(resources.getIdentifier("status_bar_height", "dimen", "android")).thenReturn(-1);
+        when(resources.getDimensionPixelSize(dim)).thenReturn(1);
+        Assert.assertEquals(0, DeviceUtils.getStatusBarHeight(context));
 
+        verify(resources, never()).getDimensionPixelSize(dim);
     }
     //cannot tested, because the creation of the TypedValue object in the method
     @Test
