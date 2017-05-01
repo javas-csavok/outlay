e17f802989fae212c270f59c1230b625cff18c2f
robin <robin.kurovszky@gmail.com>
2017-05-01 14:33:50 +0200

    method stubs on tests

--- outlay/app/src/main/java/test/TestDeviceUtils.java
+++ outlay/app/src/main/java/test/TestDeviceUtils.java
@@ -1,9 +1,47 @@
 package test;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by robin on 2017. 05. 01..
  */
 
 public class TestDeviceUtils {
-    
+
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void screenSizeTest(){
+
+    }
+    @Test
+    public void getStatusBarHeightBranchTrue(){
+
+    }
+    @Test
+    public void getStatusBarHeightBranchFalse(){
+
+    }
+    @Test
+    public void getActionBarHeightBranchTrue(){
+
+    }
+    @Test
+    public void getActionBarHeightBranchFalse(){
+
+    }
+    @Test
+    public void hideKeyboardBranchTrue(){
+        
+    }
+    @Test
+    public void hideKeyboardBranchFalse(){
+
+    }
+
 }
