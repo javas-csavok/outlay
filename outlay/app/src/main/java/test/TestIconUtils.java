b069d307ab1bad699814b14fc27463c93336910d
stahopeti <stahopeti@gmail.com>
2017-05-01 14:42:07 +0200

    Created the skeleton of the TestIconUtils class

---
+++ outlay/app/src/main/java/test/TestIconUtils.java
@@ -0,0 +1,55 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Stahorszki Péter on 2017. 05. 01..
+ */
+
+public class TestIconUtils {
+    @Before
+    public void setUp(){
+
+    }
+
+    @Test
+    public void testGetAll(){
+
+    }
+
+    @Test
+    public void loadCategoryIcon(){
+
+    }
+
+    @Test
+    public void getToolbarIcon(){
+
+    }
+
+    @Test
+    public void getIconMaterialIcon1(){
+
+    }
+
+    @Test
+    public void getIconMaterialIcon2(){
+
+    }
+
+    @Test
+    public void getToolbarIcon2(){
+
+    }
+
+    @Test
+    public void loadCategoryIcon2(){
+
+    }
+    
+    @Test
+    public void getCategoryIcon(){
+
+    }
+}
